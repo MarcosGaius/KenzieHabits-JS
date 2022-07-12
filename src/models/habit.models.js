230a4dd481e3fdb55914273f8c06e36ef1ee5e90
shirator <carlito.elm78@gmail.com>
2022-07-12 16:24:36 -0400

    feat: Creating getDoneHabits

--- src/models/habit.models.js
+++ src/models/habit.models.js
@@ -1,18 +1,33 @@
 class Habits {
   static baseUrl = "https://habits-kenzie.herokuapp.com/api";
+
   static async updateHabit(id, updateObj) {
     return await fetch(`${this.baseUrl}/habits/${id}`, {
       method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${JSON.parse(
-          localStorage.getItem("@kenziehabits:token")
-        )}`,
-      },
+      headers: this.headers,
       body: JSON.stringify(updateObj),
     })
       .then((res) => res.json())
       .then((res) => res)
       .catch((err) => console.error(err));
+  }
+
+  static async getDoneHabits() {
+    const getDone = await fetch(`${this.baseUrl}/habits`, {
+      method: "GET",
+      headers: this.headers,
+    })
+      .then((res) => res.json())
+      .then((res) => res)
+      .catch((err) => {
+        console.error(err);
+        return err;
+      });
+
+    let newData = getDone.filter((elem) => {
+      return elem.habit_status == true;
+    });
+
+    return newData;
   }
 }
