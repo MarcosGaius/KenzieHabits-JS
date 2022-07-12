dbf6ad72a253ac226fac186b71a2d1707e0f5498
Vitor Brasil <vitorlbrasil@hotmail.com>
2022-07-12 14:57:49 -0300

    feat: adding setHabitDone() method and the export declaration

--- src/models/habit.models.js
+++ src/models/habit.models.js
@@ -15,4 +15,21 @@ class Habits {
       .then((res) => res)
       .catch((err) => console.error(err));
   }
+
+  static async setHabitDone(id) {
+    return await fetch(this.baseUrl + `/habits/complete/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem("@kenziehabits:token")}`
+      }
+    })
+    .then((res) => res.json())
+    .catch((err) => {
+      console.error(err)
+      return err
+    })
+
+    //falta realizar testes
+  }
 }
