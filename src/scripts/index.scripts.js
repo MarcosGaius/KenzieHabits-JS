e26edb42e469f0da640d6382ca535d1259df0f97
Marcos Gaius <87880897+MarcosGaius@users.noreply.github.com>
2022-07-13 19:44:44 -0400

    feat: added loading container and methods to call and hide it

--- src/scripts/index.scripts.js
+++ src/scripts/index.scripts.js
@@ -2,7 +2,6 @@ import Form from "../controller/form.controllers.js";
 import Access from "../controller/access.controllers.js";
 import User from "../models/user.models.js"
 import Notification from "../controller/notification.controller.js";
-
 (
     async () => {
         const token = localStorage.getItem("@kenziehabits:token");
