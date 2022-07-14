4c6e98b28131258b241577dd803467c4fd588935
shirator <carlito.elm78@gmail.com>
2022-07-13 20:11:58 -0400

    fix: resolving bugs with the branch

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,25 +1,44 @@
+import Access from "../controller/access.controllers.js";
+
 export default class User {
   static baseUrl = "https://habits-kenzie.herokuapp.com/api";
-
   static async editUserData(editObj) {
     return await fetch(`${this.baseUrl}/user/profile`, {
       method: "PATCH",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${JSON.parse(
-          localStorage.getItem("@kenziehabits:token")
-        )}`,
+        Authorization: `Bearer ${localStorage.getItem("@kenziehabits:token")}`,
       },
       body: JSON.stringify(editObj),
     })
-      .then((res) => res.json())
-      .then((data) => data)
-      .catch((err) => console.error(err));
+      .then(async (res) => {
+        if (!res.ok) {
+          throw {
+            status: res.status,
+            statusText: res.statusText,
+            message: res.message,
+          };
+        }
+
+        res = await res.json();
+
+        localStorage.setItem(
+          "@kenziehabits:userdata",
+          JSON.stringify(res.response)
+        );
+        localStorage.setItem("@kenziehabits:token", res.token);
+
+        return res;
+      })
+      .catch((err) => {
+        return err;
+      });
   }
 
   static logUserOut() {
+    localStorage.removeItem("@kenziehabits:userdata");
     localStorage.removeItem("@kenziehabits:token");
-    Redirect.loginPage();
+    Access.redirectToLoginPage();
   }
 
   static async logUserIn(email, password) {
@@ -35,30 +54,29 @@ export default class User {
       },
       body: JSON.stringify(logInData),
     })
-      .then((res) => res.json())
-      .then((res) => {
+      .then(async (res) => {
+        const resData = await res.json();
+
+        if (!res.ok) {
+          throw {
+            status: res.status,
+            statusText: res.statusText,
+            message: resData.message,
+          };
+        }
+
         localStorage.setItem(
           "@kenziehabits:userdata",
-          JSON.stringify(res.response)
+          JSON.stringify(resData.response)
         );
-        localStorage.setItem("@kenziehabits:token", res.token);
+        localStorage.setItem("@kenziehabits:token", resData.token);
 
-        //falta método de redirecionamento
-
-        return res;
+        return resData;
       })
       .catch((err) => {
-        console.error(err);
         return err;
       });
 
     return response;
   }
 }
-
-// User.logUserIn(
-//   "grupo5RafaelK@mail.com",
-//   "aaa6cd662711478489f51cd12036a71a"
-// ).then((res) => console.log(res));
-
-// User.logUserOut()
